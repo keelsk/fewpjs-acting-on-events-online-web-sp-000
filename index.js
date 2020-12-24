73cf0ed975dc6777795d00dbb6bda2e480a69ce9
Kirsten Keels <keelsk@gmail.com>
2020-12-24 18:04:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // Your code here
-
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
 
 function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
