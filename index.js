9c509832a4521b39a6464ffd64ae04a7589629b2
Kirsten Keels <keelsk@gmail.com>
2020-12-24 18:06:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
